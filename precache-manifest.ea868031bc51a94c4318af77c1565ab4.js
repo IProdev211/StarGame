a38a8189673dabe8efc01826bc05b5df313d11ac
michael <mlositsky.dev@gmail.com>
2020-06-22 10:21:23 +1000

    Updates

---
+++ precache-manifest.ea868031bc51a94c4318af77c1565ab4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ed0cba25d4a8afbb8622d1b0b3fc8d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "6d1af6286c6bd3b4ab04",
+    "url": "/test/static/css/main.80af4cc7.chunk.css"
+  },
+  {
+    "revision": "7051abb0481add88d18a",
+    "url": "/test/static/js/2.684ce877.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/test/static/js/2.684ce877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1af6286c6bd3b4ab04",
+    "url": "/test/static/js/main.4a2863de.chunk.js"
+  },
+  {
+    "revision": "31f88d222cc0d46c98d0",
+    "url": "/test/static/js/runtime-main.1804cb34.js"
+  },
+  {
+    "revision": "3d7f7a6c383274b81e83a954f71100db",
+    "url": "/test/static/media/background.3d7f7a6c.png"
+  },
+  {
+    "revision": "be7def752a208652da0f99c1aef4ea4f",
+    "url": "/test/static/media/ship.be7def75.png"
+  }
+]);
